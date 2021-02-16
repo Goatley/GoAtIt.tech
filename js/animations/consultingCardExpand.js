781023345e3bda5e633ce8b2891103b55f33c454
Goat$ <adam.goatley@goatit.tech>
2021-02-16 15:46:16 -0500

    a

--- js/animations/consultingCardExpand.js
+++ js/animations/consultingCardExpand.js
@@ -126,8 +126,7 @@ export function closeMobileCard() {
     
     gsap.to('.mobileExpandedCard', {
         duration: aniDuration,
-        x: '+=100vw',
-        onComplete: () => {gsap.set('.mobileExpandedCard', {x: '-150vw'})}
+        x: '100vw'
     });
     gsap.to('.mobileCardOverlay', {
         duration: aniDuration / 2,
