d1b2f7224c7c952c93afc07a3c7cfd2c65b4c862
Goat$ <adam.goatley@goatit.tech>
2021-02-17 21:24:08 -0500

    a

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     plugins: [
       require('tailwindcss'),
-      require('autoprefixer'),
+      // require('autoprefixer'),
       require('postcss-preset-env'),
       require('postcss-nesting'),
     ]
